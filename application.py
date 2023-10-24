fd883d1cc139a739b2fdefe1a4cf2079f50b105a
Ferid Dohru <doghru98@gmail.com>
2023-10-24 13:47:24 +0300

    Adding setup_logging function that setups console and file logging

--- application.py
+++ application.py
@@ -1,4 +1,24 @@
 import argparse
+import logging
+
+
+def setup_logging(log_file):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    # Create a file handler for the log file
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    # Create a console handler for console output
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Folder synchronization program")
@@ -8,5 +28,3 @@ if __name__ == "__main__":
     parser.add_argument("--log_file", default="logs/sync.log", help="Log file path (default: logs folder and sync.log)")
 
     args = parser.parse_args()
-
-
